2d007e47d131c7686eef5a86d4c0117c433b0400
CCchao888 <wjc510175817@163.com>
2024-01-21 22:38:36 +0800

    创建各个子模块，各个包与yaml文件

---
+++ admin/src/main/java/com/chao/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package com.chao.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author:chao
+ * Date:2024-01-21
+ * Description:
+ */
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
