a3f0ee9d77063192e7bd7021e5b6d05cafc301a9
CCchao888 <wjc510175817@163.com>
2024-01-28 21:32:56 +0800

    feature:开发mybatisPlus的分页插件

---
+++ project/src/main/java/com/chao/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,27 @@
+package com.chao.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author:chao
+ * Date:2024-01-28
+ * Description: 数据库配置类
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * MybatisPlus 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
