b21db1f99896b3129fba81009fdc328fce841d3c
CCchao888 <wjc510175817@163.com>
2024-02-18 23:06:05 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/com/chao/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,13 @@
+package com.chao.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chao.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * Author:chao
+ * Date:2024-02-18
+ * Description: 短链接今日统计持久层
+ */
+
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
