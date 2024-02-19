6e31b8c7e748e156ff0eb89d69058fb92d9f336b
CCchao888 <wjc510175817@163.com>
2024-02-19 12:05:04 +0800

    feature: 开发短链接监控之今日统计

--- project/src/main/java/com/chao/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/chao/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,7 +2,10 @@ package com.chao.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.chao.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -15,6 +18,9 @@ import java.util.Date;
 
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -50,5 +56,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * 今日ip数
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
--- project/src/main/java/com/chao/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
+++ project/src/main/java/com/chao/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -2,6 +2,8 @@ package com.chao.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.chao.shortlink.project.dao.entity.LinkStatsTodayDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Author:chao
@@ -10,4 +12,13 @@ import com.chao.shortlink.project.dao.entity.LinkStatsTodayDO;
  */
 
 public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+
+    /**
+     * 记录今日统计监控数
+     */
+    @Insert("INSERT INTO t_link_stats_today (full_short_url, gid, date,  today_uv, today_pv, today_uip, create_time, update_time, del_flag) " +
+            "VALUES( #{linkTodayStats.fullShortUrl}, #{linkTodayStats.gid}, #{linkTodayStats.date}, #{linkTodayStats.todayUv}, #{linkTodayStats.todayPv}, #{linkTodayStats.todayUip}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE today_uv = today_uv +  #{linkTodayStats.todayUv}, today_pv = today_pv +  #{linkTodayStats.todayPv}, today_uip = today_uip +  #{linkTodayStats.todayUip};")
+    void shortLinkTodayState(@Param("linkTodayStats") LinkStatsTodayDO linkStatsTodayDO);
+
 }
