f822a55644260f49c692a85fcb3b52dce5e99653
CCchao888 <wjc510175817@163.com>
2024-02-21 15:22:22 +0800

    feature: 短链接后管通过风控保障系统稳定性

--- admin/src/main/java/com/chao/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/chao/shortlink/admin/config/UserConfiguration.java
@@ -1,7 +1,9 @@
 package com.chao.shortlink.admin.config;
 
 
+import com.chao.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.chao.shortlink.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,5 +28,21 @@ public class UserConfiguration {
         registration.setOrder(0);
         return registration;
     }
+
+    /**
+     * 用户操作流量风控过滤器
+     */
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(10);
+        return registration;
+    }
+
 }
 
